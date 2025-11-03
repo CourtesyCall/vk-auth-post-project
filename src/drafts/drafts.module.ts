cb06e3515e9cb1fe4e8f49574f5988f8a1ff509c
CourtesyCall <zamirskyi20@gmail.com>
2025-11-03 17:00:45 +0200

    fixed draft error

--- src/drafts/drafts.module.ts
+++ src/drafts/drafts.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DraftsService } from './drafts.service';
 import { DraftsController } from './drafts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Draft } from './entities/draft.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Draft]) // <-- 3. ДОБАВЬТЕ ЭТУ СТРОКУ
+  ],
   controllers: [DraftsController],
   providers: [DraftsService],
 })
